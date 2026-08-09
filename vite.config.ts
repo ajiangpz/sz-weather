30bb85b4cc6d3f74e0e03b855d99f58710e08c35
ajiangpz <1174072083@qq.com>
2026-08-09 15:08:08 +0800

    ci: make Vite base path deployment-specific

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/sz-weather/' : '/',
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [vue()],
   resolve: {
     alias: {
